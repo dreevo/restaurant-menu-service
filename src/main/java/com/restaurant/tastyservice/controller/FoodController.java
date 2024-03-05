43b3b941047086d26a2cb193ca15637ebb85fe76
dreevo <sidedark85@gmail.com>
2024-03-05 12:11:17 +0100

    metrics health

--- src/main/java/com/restaurant/tastyservice/controller/FoodController.java
+++ src/main/java/com/restaurant/tastyservice/controller/FoodController.java
@@ -5,6 +5,8 @@ import com.restaurant.tastyservice.domain.FoodService;
 import javax.validation.Valid;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,34 +24,43 @@ public class FoodController {
 
     private final FoodService foodService;
 
+    private static final Logger log =
+            LoggerFactory.getLogger(FoodController.class);
+
     public FoodController(FoodService foodService) {
         this.foodService = foodService;
     }
 
     @GetMapping
     public Iterable<Food> get() {
+        log.info("Fetching the list of food in the menu");
+
         return foodService.viewFoodList();
     }
 
     @GetMapping("{ref}")
     public Food getByRef(@PathVariable String ref) {
+        log.info("Fetching the food with ref {} from the catalog", ref);
         return foodService.viewFoodDetails(ref);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Food post(@Valid @RequestBody Food food) {
+        log.info("Adding a new food to the menu with ref {}", food.ref());
         return foodService.addFoodToMenu(food);
     }
 
     @DeleteMapping("{ref}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String ref) {
+        log.info("Deleting food with ref {}", ref);
         foodService.deleteFoodFromMenu(ref);
     }
 
     @PutMapping("{ref}")
     public Food put(@PathVariable String ref, @Valid @RequestBody Food food) {
+        log.info("Updating food with ref {}", ref);
         return foodService.editFoodDetails(food, ref);
     }
 }
